62586e5092ed751eb03e97ac1d3207380aab0dd6
Kevin Tan <kevin.tan97@hotmail.com>
2018-01-28 17:48:34 -0500

    added format.xml, and vote/forum post class

--- src/main/java/backend/model/qa/common/ForumPost.java
+++ src/main/java/backend/model/qa/common/ForumPost.java
@@ -1,5 +1,6 @@
 package backend.model.qa.common;
 
+import backend.model.vote.VoteModel;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 
@@ -12,16 +13,15 @@ public abstract class ForumPost {
     protected String postMessage;
     protected DateTime dateTime;
     //TODO figure out if Vote model should be implemented to hold these values issue#12
-    protected int upVotes, downVotes;
+    protected final VoteModel votes;
     //TODO replace with User
     protected String author;
 
-    public ForumPost(String postMessage, String author, DateTime dateTime, int upVotes, int downVotes) {
+    public ForumPost(String postMessage, String author, DateTime dateTime, VoteModel votes) {
         this.postMessage = postMessage;
         this.author = author;
         this.dateTime = dateTime;
-        this.upVotes = upVotes;
-        this.downVotes = downVotes;
+        this.votes = votes;
     }
 
     public String getPostMessage() {
@@ -32,11 +32,39 @@ public abstract class ForumPost {
         return dateTime.toString(DateTimeFormat.mediumDateTime());
     }
 
+    public int getUpVotes(){
+        return votes.getUpVotes();
+    }
+
+    public int getTotalVotes(){
+        return votes.getTotalVotes();
+    }
+
+    public int getDownVotes(){
+        return votes.getDownVotes();
+    }
+
     public void setPostMessage(String postMessage) {
         this.postMessage = postMessage;
     }
 
     public void setDateTime(DateTime dateTime) {
         this.dateTime = dateTime;
+    }
+
+    public void incrementUpVote(){
+        votes.incrementDownVotes();
+    }
+
+    public void decrementUpVote(){
+        votes.decrementUpVotes();
+    }
+
+    public void incrementDownVote(){
+        votes.incrementDownVotes();
+    }
+
+    public void decrementDownVote(){
+        votes.decrementDownVotes();
     }
 }

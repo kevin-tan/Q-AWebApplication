818b49aa8d5d873a801fbbcb2b57e5f5eaba4ccc
SagarPatel43 <patelsagar1997@gmail.com>
2018-01-30 21:59:16 -0500

    commiting before push

---
+++ src/main/java/backend/controller/UserAnswerController.java
@@ -0,0 +1,8 @@
+package backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/[userId]/questions/[questionId]/replies")
+public class UserAnswerController {}

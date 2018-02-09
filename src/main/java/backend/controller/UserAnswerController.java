8d04391a951691ff13f4f9e9d6e4fb0c59382a81
Kevin Tan <kevin.tan97@hotmail.com>
2018-02-08 22:35:30 -0500

    fixed answermodel bug, not having a votemodel

--- src/main/java/backend/controller/UserAnswerController.java
+++ src/main/java/backend/controller/UserAnswerController.java
@@ -2,9 +2,11 @@ package backend.controller;
 
 import backend.model.qa.AnswerModel;
 import backend.model.user.UserModel;
+import backend.model.vote.VoteModel;
 import backend.repository.qa.AnswerRepository;
 import backend.repository.qa.QuestionRepository;
 import backend.repository.user.UserRepository;
+import backend.repository.vote.VoteRepository;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,27 +19,33 @@ import static backend.controller.constants.ForumPostConstants.JSON;
 @RequestMapping("/user/{userId}/questions/{questionId}")
 public class UserAnswerController {
 
-    private UserRepository userRepository;
-    private QuestionRepository questionRepository;
-    private AnswerRepository answerRepository;
+    private final UserRepository userRepository;
+    private final QuestionRepository questionRepository;
+    private final AnswerRepository answerRepository;
+    private final VoteRepository voteRepository;
 
     @Autowired
-    public UserAnswerController(UserRepository userRepository, QuestionRepository questionRepository, AnswerRepository answerRepository) {
+    public UserAnswerController(UserRepository userRepository, QuestionRepository questionRepository, AnswerRepository answerRepository,
+                                VoteRepository voteRepository) {
         this.userRepository = userRepository;
         this.questionRepository = questionRepository;
         this.answerRepository = answerRepository;
+        this.voteRepository = voteRepository;
     }
 
     @PostMapping(value = "/replies", consumes = JSON, produces = JSON)
-    public void postReply(@PathVariable long questionId, @PathVariable long userId, @RequestBody AnswerModel answerModel) {
+    public AnswerModel postReply(@PathVariable long questionId, @PathVariable long userId, @RequestBody AnswerModel answerModel) {
         UserModel user = (userRepository.findOne(userId));
         DateTime dateTime = new DateTime();
         answerModel.setPostedDate(dateTime.toString(FORMAT));
         answerModel.setUpdatedTime(dateTime.toString(FORMAT));
         answerModel.setQuestion(questionRepository.findOne(questionId));
         answerModel.setUserAnswer(user);
+        answerModel.setVotes(new VoteModel(answerModel, user));
         user.incrementReputation();
         answerRepository.save(answerModel);
+        voteRepository.save(answerModel.getVotes());
+        return answerModel;
     }
 
     @PutMapping(value = "/replies/{replyId}", consumes = JSON, produces = JSON)

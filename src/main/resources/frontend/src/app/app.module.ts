bc5599c2723abd9223bb9de630fd53f6a11d29f1
kj666 <karthi-kan@hotmail.com>
2018-02-09 16:13:35 -0500

    Import the routes for component

--- src/main/resources/frontend/src/app/app.module.ts
+++ src/main/resources/frontend/src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from "@angular/router";
 
 
 import { AppComponent } from './app.component';
 import { QuestionsComponent } from './questions/questions.component';
 import { HttpClientModule} from "@angular/common/http";
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+const appRoutes: Routes =[
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent}
+
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionsComponent
+    QuestionsComponent,
+    HeaderComponent,
+    LoginComponent,
+    FooterComponent,
+    HomeComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes,{enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]

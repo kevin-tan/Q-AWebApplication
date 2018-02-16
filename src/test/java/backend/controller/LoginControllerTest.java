d38b484d68746889633924eb52ef38b19915ace1
SagarPatel43 <patelsagar1997@gmail.com>
2018-02-15 22:06:07 -0500

    Added all tests cases for LoginController and RegisterController

--- src/test/java/backend/controller/LoginControllerTest.java
+++ src/test/java/backend/controller/LoginControllerTest.java
@@ -1,9 +1,14 @@
 package backend.controller;
 
 
+import backend.model.roles.RoleModel;
 import backend.model.user.UserModel;
+import backend.repository.roles.RolesRepository;
 import backend.repository.user.UserRepository;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,9 +23,10 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
 
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
@@ -30,9 +36,6 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @ActiveProfiles("test")
 @WebAppConfiguration
 public class LoginControllerTest {
-    //TODO Need to test posting your login info and seeing if it verifies when it's correct, and denies when you're wrong
-
-    private final String EMPTY = "";
 
     private MediaType mediaType =
             new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
@@ -40,51 +43,62 @@ public class LoginControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
-    private UserRepository userRepository;
-    @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private RolesRepository rolesRepository;
+    @Autowired
+    private UserRepository userRepository;
     @Autowired
     private WebApplicationContext webApplicationContext;
     @Autowired
     private MappingJackson2HttpMessageConverter jackson2HttpMessageConverter;
 
+    private UserModel registerUser;
+
     private UserModel successfulLogin = new UserModel("foo", "bar", "", "", "");
     private UserModel invalidUser = new UserModel("fooINVALID", "bar", "", "", "");
     private UserModel invalidPassword = new UserModel("foo", "barINVALID", "", "", "");
+    private UserModel invalidUserPassword = new UserModel("fooINVALID", "barINVALID", "", "", "");
+
+    private RoleModel adminRole;
+    private RoleModel userRole;
 
     @Before
+    @SuppressWarnings("Duplicates")
     public void setUp() {
         mockMvc = webAppContextSetup(webApplicationContext).build();
-        UserModel user = new UserModel("foo", bCryptPasswordEncoder.encode("bar"), "name", "lastname", "foo@bar.com");
-        userRepository.save(user);
+
+        adminRole = new RoleModel("admin");
+        userRole = new RoleModel("user");
+        rolesRepository.save(adminRole);
+        rolesRepository.save(userRole);
+
+        registerUser = new UserModel("foo", bCryptPasswordEncoder.encode("bar"), "name", "lastname", "foo@bar.com");
+        userRepository.save(registerUser);
     }
 
-/*    @Test
-    public void successfulLogin_thenReturnUserStoredInDatabase() throws Exception {
-        ObjectMapper objectMapper = jackson2HttpMessageConverter.getObjectMapper();
+    @After
+    @SuppressWarnings("Duplicates")
+    public void tearDown() {
+        rolesRepository.delete(adminRole.getId());
+        rolesRepository.delete(userRole.getId());
 
-        mockMvc.perform(post("/login").contentType(mediaType)
-                .content(objectMapper.writeValueAsString(successfulLogin)))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(mediaType))
-                .andExpect(jsonPath("$.username", is(user.getUsername())))
-                .andExpect(jsonPath("$.firstName", is(user.getFirstName())))
-                .andExpect(jsonPath("$.lastName", is(user.getLastName())))
-                .andExpect(jsonPath("$.email", is(user.getEmail())));
-    }*/
+        userRepository.delete(registerUser);
+    }
 
     @Test
-    public void unsuccessfulLoginInvalidUser_thenReturnEmptyJson() throws Exception {
+    public void unsuccessfulLoginInvalidUserPassword_thenReturnEmptyJson() throws Exception {
         ObjectMapper objectMapper = jackson2HttpMessageConverter.getObjectMapper();
 
         mockMvc.perform(post("/login").contentType(mediaType)
-                .content(objectMapper.writeValueAsString(invalidUser)))
+                .content(getContent(objectMapper, invalidUserPassword)))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(mediaType))
-                .andExpect(jsonPath("$.username", is(EMPTY)))
-                .andExpect(jsonPath("$.firstName", is(EMPTY)))
-                .andExpect(jsonPath("$.lastName", is(EMPTY)))
-                .andExpect(jsonPath("$.email", is(EMPTY)));
+                .andExpect(jsonPath("$.id", nullValue()))
+                .andExpect(jsonPath("$.username", isEmptyString()))
+                .andExpect(jsonPath("$.firstName", isEmptyString()))
+                .andExpect(jsonPath("$.lastName", isEmptyString()))
+                .andExpect(jsonPath("$.email", isEmptyString()));
     }
 
     @Test
@@ -92,13 +106,53 @@ public class LoginControllerTest {
         ObjectMapper objectMapper = jackson2HttpMessageConverter.getObjectMapper();
 
         mockMvc.perform(post("/login").contentType(mediaType)
-                .content(objectMapper.writeValueAsString(invalidPassword)))
+                .content(getContent(objectMapper, invalidPassword)))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(mediaType))
-                .andExpect(jsonPath("$.username", is(EMPTY)))
-                .andExpect(jsonPath("$.firstName", is(EMPTY)))
-                .andExpect(jsonPath("$.lastName", is(EMPTY)))
-                .andExpect(jsonPath("$.email", is(EMPTY)));
+                .andExpect(jsonPath("$.id", nullValue()))
+                .andExpect(jsonPath("$.username", isEmptyString()))
+                .andExpect(jsonPath("$.firstName", isEmptyString()))
+                .andExpect(jsonPath("$.lastName", isEmptyString()))
+                .andExpect(jsonPath("$.email", isEmptyString()));
+    }
+
+
+    @Test
+    public void unsuccessfulLoginInvalidUser_thenReturnEmptyJson() throws Exception {
+        ObjectMapper objectMapper = jackson2HttpMessageConverter.getObjectMapper();
+
+        mockMvc.perform(post("/login").contentType(mediaType)
+                .content(getContent(objectMapper, invalidUser)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(mediaType))
+                .andExpect(jsonPath("$.id", nullValue()))
+                .andExpect(jsonPath("$.username", isEmptyString()))
+                .andExpect(jsonPath("$.firstName", isEmptyString()))
+                .andExpect(jsonPath("$.lastName", isEmptyString()))
+                .andExpect(jsonPath("$.email", isEmptyString()));
+    }
+
+    @Test
+    public void successfulLogin_thenReturnUserStoredInDatabase() throws Exception {
+        ObjectMapper objectMapper = jackson2HttpMessageConverter.getObjectMapper();
+
+        mockMvc.perform(post("/login").contentType(mediaType)
+                .content(getContent(objectMapper, successfulLogin)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(mediaType))
+                .andExpect(jsonPath("$.id", notNullValue()))
+                .andExpect(jsonPath("$.username", is(registerUser.getUsername())))
+                .andExpect(jsonPath("$.firstName", is(registerUser.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(registerUser.getLastName())))
+                .andExpect(jsonPath("$.email", is(registerUser.getEmail())));
+    }
+
+
+    //Used to bypass password attribute being ignored by ObjectMapper
+    private String getContent(ObjectMapper objectMapper, UserModel userModel) throws IOException {
+        JsonNode kms = objectMapper.readTree(objectMapper.writeValueAsString(userModel));
+        ((ObjectNode) kms).put("password", userModel.getPassword());
+        return objectMapper.writeValueAsString(kms);
     }
 
 }
--- src/test/java/backend/controller/RegisterControllerTest.java
+++ src/test/java/backend/controller/RegisterControllerTest.java
@@ -1,7 +1,13 @@
 package backend.controller;
 
+import backend.model.roles.RoleModel;
 import backend.model.user.UserModel;
+import backend.repository.roles.RolesRepository;
+import backend.repository.user.UserRepository;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,9 +21,10 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
 
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
@@ -37,14 +44,41 @@ public class RegisterControllerTest {
     private WebApplicationContext webApplicationContext;
     @Autowired
     private MappingJackson2HttpMessageConverter jackson2HttpMessageConverter;
+    @Autowired
+    private RolesRepository rolesRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-    private UserModel registerUser;
+    private final UserModel registerValidUser = new UserModel("foo", "bar", "name", "lastname", "foo@bar.com");
+
+    private final UserModel validateUser = new UserModel("user", "pass", "name", "lastname", "email@test.com");
+    private final UserModel invalidEmail = new UserModel("userNEW", "pass", "name", "lastname", "email@test.com");
+    private final UserModel invalidUsername = new UserModel("user", "pass", "name", "lastname", "emailNEW@test.com");
+    private final UserModel invalidUsernameEmail = new UserModel("user", "pass", "name", "lastname", "email@test.com");
+
+    private RoleModel adminRole;
+    private RoleModel userRole;
 
     @Before
+    @SuppressWarnings("Duplicates")
     public void setUp() {
         mockMvc = webAppContextSetup(webApplicationContext).build();
 
-        registerUser = new UserModel("foo", "bar", "name", "lastname", "foo@bar.com");
+        adminRole = new RoleModel("admin");
+        userRole = new RoleModel("user");
+        rolesRepository.save(adminRole);
+        rolesRepository.save(userRole);
+
+        userRepository.save(validateUser);
+    }
+
+    @After
+    @SuppressWarnings("Duplicates")
+    public void tearDown() {
+        rolesRepository.delete(adminRole.getId());
+        rolesRepository.delete(userRole.getId());
+
+        userRepository.delete(validateUser);
     }
 
     @Test
@@ -52,14 +86,65 @@ public class RegisterControllerTest {
         ObjectMapper objectMapper = jackson2HttpMessageConverter.getObjectMapper();
 
         mockMvc.perform(post("/register").contentType(mediaType)
-                .content(objectMapper.writeValueAsString(registerUser)))
+                .content(getContent(objectMapper, registerValidUser)))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(mediaType))
-                .andExpect(jsonPath("$.username", is(registerUser.getUsername())))
-                .andExpect(jsonPath("$.firstName", is(registerUser.getFirstName())))
-                .andExpect(jsonPath("$.lastName", is(registerUser.getLastName())))
-                .andExpect(jsonPath("$.email", is(registerUser.getEmail())));
+                .andExpect(jsonPath("$.id", is(notNullValue())))
+                .andExpect(jsonPath("$.username", is(registerValidUser.getUsername())))
+                .andExpect(jsonPath("$.firstName", is(registerValidUser.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(registerValidUser.getLastName())))
+                .andExpect(jsonPath("$.email", is(registerValidUser.getEmail())));
     }
 
+    @Test
+    public void unsuccessfulRegisterInvalidEmail_thenReturnUserModelEmptyEmailNullId() throws Exception {
+        ObjectMapper objectMapper = jackson2HttpMessageConverter.getObjectMapper();
 
+        mockMvc.perform(post("/register").contentType(mediaType)
+                .content(getContent(objectMapper, invalidEmail)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(mediaType))
+                .andExpect(jsonPath("$.id", is(nullValue())))
+                .andExpect(jsonPath("$.username", is(invalidEmail.getUsername())))
+                .andExpect(jsonPath("$.firstName", is(invalidEmail.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(invalidEmail.getLastName())))
+                .andExpect(jsonPath("$.email", is(EMPTY)));
+    }
+
+    @Test
+    public void unsuccessfulRegisterInvalidUsernameInvalidEmail_thenReturnUserModelEmptyUsernameEmptyEmailNullId() throws Exception {
+        ObjectMapper objectMapper = jackson2HttpMessageConverter.getObjectMapper();
+
+        mockMvc.perform(post("/register").contentType(mediaType)
+                .content(getContent(objectMapper, invalidUsernameEmail)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(mediaType))
+                .andExpect(jsonPath("$.id", is(nullValue())))
+                .andExpect(jsonPath("$.username", is(EMPTY)))
+                .andExpect(jsonPath("$.firstName", is(invalidUsernameEmail.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(invalidUsernameEmail.getLastName())))
+                .andExpect(jsonPath("$.email", is(EMPTY)));
+    }
+
+    @Test
+    public void unsuccessfulRegisterInvalidUsername_thenReturnUserModelEmptyUsernameNullId() throws Exception {
+        ObjectMapper objectMapper = jackson2HttpMessageConverter.getObjectMapper();
+
+        mockMvc.perform(post("/register").contentType(mediaType)
+                .content(getContent(objectMapper, invalidUsername)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(mediaType))
+                .andExpect(jsonPath("$.id", is(nullValue())))
+                .andExpect(jsonPath("$.username", is(EMPTY)))
+                .andExpect(jsonPath("$.firstName", is(invalidUsername.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(invalidUsername.getLastName())))
+                .andExpect(jsonPath("$.email", is(invalidUsername.getEmail())));
+    }
+
+    //Used to bypass password attribute being ignored by ObjectMapper
+    private String getContent(ObjectMapper objectMapper, UserModel userModel) throws IOException {
+        JsonNode kms = objectMapper.readTree(objectMapper.writeValueAsString(userModel));
+        ((ObjectNode) kms).put("password", userModel.getPassword());
+        return objectMapper.writeValueAsString(kms);
+    }
 }

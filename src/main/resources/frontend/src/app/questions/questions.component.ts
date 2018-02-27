905959ddc1776f6e8ca091ef0df0b003de26aa77
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2018-02-26 23:15:40 -0500

    Added functionality in which only users can post question

--- src/main/resources/frontend/src/app/questions/questions.component.ts
+++ src/main/resources/frontend/src/app/questions/questions.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Question} from "./question";
 import {QuestionsService} from "./questions.service";
 import {Answer} from "./answer";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-questions',
@@ -10,9 +11,10 @@ import {Answer} from "./answer";
 })
 export class QuestionsComponent implements OnInit {
 
+  displayAnswerBox:boolean = (sessionStorage.getItem('status') == 'true');
   currentQuestion: Question;
 
-  constructor(private questionsService: QuestionsService) { }
+  constructor(private questionsService: QuestionsService, private router: Router) { }
 
   ngOnInit() {
     this.questionsService.currentQuestion.subscribe(currentQuestion => this.currentQuestion = currentQuestion)
@@ -24,6 +26,14 @@ export class QuestionsComponent implements OnInit {
     const newAnswer: Answer = { message } as Answer;
     this.questionsService.addAnswerToQuestion(newAnswer, this.currentQuestion.id)
       .subscribe(answer => this.currentQuestion.answerModel.push(answer));
+  }
+
+  registerButtonClick(){
+    this.router.navigate(['/register']);
+  }
+
+  loginButtonClick(){
+    this.router.navigate(['/login']);
   }
 
 }

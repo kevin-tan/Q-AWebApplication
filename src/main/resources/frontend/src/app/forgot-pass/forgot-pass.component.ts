d4e6444ccf1a2f83caf1e2ec2e63437659b4b9ef
kj666 <karthi-kan@hotmail.com>
2018-02-26 22:19:02 -0500

    forgot password - ask email

--- src/main/resources/frontend/src/app/forgot-pass/forgot-pass.component.ts
+++ src/main/resources/frontend/src/app/forgot-pass/forgot-pass.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-forgot-pass',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPassComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  submitEmail(){
+    this.router.navigateByUrl('/loginQuestion');
+  }
 }

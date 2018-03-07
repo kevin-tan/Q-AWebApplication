42b00665896b2c515f25486e30eb4f8717052115
Shane Phillips <shanerobphillips@gmail.com>
2018-03-06 20:34:20 -0500

    did something wrong

--- src/main/resources/frontend/src/app/dashboard/dashboard.component.ts
+++ src/main/resources/frontend/src/app/dashboard/dashboard.component.ts
@@ -10,7 +10,6 @@ import { Router } from '@angular/router'
 export class DashboardComponent implements OnInit {
 
   public questions = [];
-  public tags = ["Category 1","Category 2","Category 3","Category 4","Category 5","Category 6","Category 7"];
 
   constructor(private questionsService: QuestionsService, private router: Router) { }
 

42ab10e656e610a9c0d17b1ae2fc2a356820353c
kj666 <karthi-kan@hotmail.com>
2018-03-20 15:28:25 -0400

    update on the chrome headless

--- src/main/resources/frontend/karma.conf.js
+++ src/main/resources/frontend/karma.conf.js
@@ -27,13 +27,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     customLaunchers: {
-      // chrome setup for travis CI using chromium
-      Chrome_travis_ci: {
-        base: 'Chrome',
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
         flags: ['--no-sandbox']
-      }},
+      }
+    },
     singleRun: true
   });
 };

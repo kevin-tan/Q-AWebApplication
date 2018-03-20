82f7d0263d74a9835c646409cacd642559836d20
kj666 <karthi-kan@hotmail.com>
2018-03-20 15:16:31 -0400

    setup chrome for travis

--- src/main/resources/frontend/karma.conf.js
+++ src/main/resources/frontend/karma.conf.js
@@ -28,6 +28,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      // chrome setup for travis CI using chromium
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }},
     singleRun: true
   });
 };

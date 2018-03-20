60c0902194e13c5342ec188e7354e7e566e2d79f
kj666 <karthi-kan@hotmail.com>
2018-03-20 15:40:29 -0400

    travis fully configured properly

--- src/main/resources/frontend/karma.conf.js
+++ src/main/resources/frontend/karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -40,21 +37,4 @@ module.exports = function (config) {
     config.browsers = ['ChromeHeadlessNoSandbox'];
   }
 };
-// module.exports = function(config) {
-//   config.set({
-//
-//     autoWatch: false,
-//
-//     browsers: ['ChromeHeadless'],
-//     // you can define custom flags
-//     customLaunchers: {
-//       ChromeHeadlessNoSandbox: {
-//         base: 'ChromeHeadless',
-//         flags: ['--no-sandbox']
-//       }
-//     },
-//
-//     singleRun: true
-//
-//   })
-// }
+

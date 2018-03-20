abba8fd2b5f0db46c64550422b95c393d6e5d40e
kj666 <karthi-kan@hotmail.com>
2018-03-20 14:49:39 -0400

    fix the header unit test

--- src/main/resources/frontend/karma.conf.js
+++ src/main/resources/frontend/karma.conf.js
@@ -26,15 +26,9 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: ['ChromeHeadless'],
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
-    singleRun: false
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: true
   });
 };
 // module.exports = function(config) {
--- src/main/resources/frontend/src/app/header/header.component.spec.ts
+++ src/main/resources/frontend/src/app/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {HeaderComponent} from './header.component';
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,6 +9,7 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ HeaderComponent ]
     })
     .compileComponents();

19867db58e06d5b70692a8a80730ddc6de6b764e
Kevin Tan <kevin.tan97@hotmail.com>
2018-03-20 20:10:54 -0400

    setup for integration test

---
+++ src/test/java/backend/integration/BackendIntegrationTest.java
@@ -0,0 +1,48 @@
+package backend.integration;
+
+
+import backend.repository.roles.RolesRepository;
+import backend.repository.user.UserRepository;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.Charset;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+@WebAppConfiguration
+public class BackendIntegrationTest {
+
+    private MediaType mediaType =
+            new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @Autowired
+    private RolesRepository rolesRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Before
+    public void setUp() {
+        mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @After
+    public void tearDown(){
+
+    }
+}
